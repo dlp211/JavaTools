90c492033fb6f63dd60210e517132f253a38be01
David Patrzeba <david.patrzeba@gmail.com>
2013-01-07 15:25:42 -0500

    updated liscense format, moved return in readHiddenInt

--- IO.java
+++ IO.java
@@ -1,30 +1,35 @@
-/*
- *    FILE: IO.java
- *    AUTHOR: David L Patrzeba
- *    E-MAIL: david.patrzeba@gmail.com
- *
- *    This file is not production code and is merely an academic exercise; that
- *    said, if you find something useful you may use this code in its entirety under the following license:
- *
- *    The MIT Liscense
- *    Copyright (c) 2012 David L Patrzeba
- *
- *    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- *    associated documentation files (the "Software"), to deal in the Software without restriction, including
- *    without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *    copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
- *    following conditions:
- *
- *    The above copyright notice and this permission notice shall be included in all
- *    copies or substantial portions of the Software.
- *
- *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
- *    LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
- *    EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- *    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
- *    THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
+/******************************************************************************
+ *    FILE: IO.java                                                           *
+ *    AUTHOR: David L Patrzeba                                                *
+ *    E-MAIL: david.patrzeba@gmail.com                                        *
+ *                                                                            *
+ *    This file is not production code and is merely an academic exercise;    *
+ *    that said, if you find something useful you may use this code in its    *
+ *    entirety under the following license:                                   *
+ *                                                                            *
+ *    The MIT Liscense                                                        *
+ *    Copyright (c) 2012 David L patrzeba                                     *
+ *                                                                            *
+ *    Permission is hereby granted, free of charge, to any person obtaining a *
+ *    copy of this software and associated documentation files (the           *
+ *    "Software"), to deal in the Software without restriction, including     *
+ *    without limitation the rights to use, copy, modify, merge, publish,     *
+ *    distribute, sublicense, and/or sell copies of the Software, and to      *
+ *    permit persons to whom the Software is furnished to do so, subject to   *
+ *    the following conditions:                                               *
+ *                                                                            *
+ *    The above copyright notice and this permission notice shall be included *
+ *    in all copies or substantial portions of the Software.                  *
+ *                                                                            *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS *
+ *    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              *
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  *
+ *    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    *
+ *    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    *
+ *    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       *
+ *    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  *
+ *                                                                            *
+ ******************************************************************************/
 
 import java.io.Console;
 import java.io.InputStreamBuffer;
@@ -34,7 +39,7 @@ import java.io.IOException;
 public class IO {
 
    private static BufferedReader input = 
-      new BufferedReader(new InputStreamBuffer(System.in));
+      new BufferedReader(new InputStreamBuffer(System.in));   
 
    private IO() {
       //should never be called
@@ -54,7 +59,6 @@ public class IO {
             System.err.println("Unexpected IO Error " + e);
          }
       }
-
    }
 
     public static long readLong(String prompt) {
@@ -92,24 +96,17 @@ public class IO {
       Console console = System.console();
       char[] chInput;
       String strInput;
-      int numberInput;
 
       while(true) {
 
-         chInput = console.readPassword(prompt + ": ");
-
          try {
+            chInput = console.readPassword(prompt + ": ");
             strInput = new String(chInput);
-            numberInput = Integer.parseInt(strInput);
-            break;
+            return Integer.parseInt(strInput);
          } catch(Exception e) {
            System.err.println("Not a valid number: ");
          }
-
       }
-
-      return mumberInput;
-
    }
 
 }
